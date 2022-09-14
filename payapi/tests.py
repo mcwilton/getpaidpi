a1c796186307596ccd9d696cfd61c780e7886b51
mcwilton <mcwilton85@gmail.com>
2022-09-14 16:28:20 +0400

    initial test file

--- payapi/tests.py
+++ payapi/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+from .models import Customers, Accounts, Transactions, Transfers
+from django.test import RequestFactory
+from django.urls import reverse
+from model_mommy import mommy
+from django.test import Client
+from .views import Scheme_API
 
-# Create your tests here.
+
+class TestCustomers(TestCase):
+    def setUp(self):
+        self.models = mommy.make('payapi.Customers')
+
+    def test_str(self):
+        self.assertEquals(str(self.models), self.models.customer_name)
+
+
+class TestAccounts(TestCase):
+    def setUp(self):
+        self.models = mommy.make('payapi.Accounts')
+
+    def test_str(self):
+        self.assertEquals(str(self.models), self.models.card_id)
+
+
+class TestTransactions(TestCase):
+    def setUp(self):
+        self.models = mommy.make('payapi.Transactions')
+
+
+class TestSchemeAPIView(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.payapi_model = mommy.make('payapi.Accounts')
+        self.customers_models = mommy.make('payapi.Customers', account=self.payapi_model, _quantity=3)
+
+
+class TestSchemeAPIIntegration(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.payapi_model = mommy.make('payapi.Accounts')
+        self.customers_models = mommy.make('payapi.Customers', account=self.payapi_model, _quantity=3)
+
+
+
